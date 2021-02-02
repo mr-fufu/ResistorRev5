d2551b0b679b6468c15e38a8e8a51328773958e2
Jack X <monkeymanp8@gmail.com>
2021-02-02 00:15:28 -0500

    fix loading fade

--- Assets/Scripts/UI_SCRIPTS/LoadingFade.cs
+++ Assets/Scripts/UI_SCRIPTS/LoadingFade.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class LoadingFade : MonoBehaviour
 {
-    public bool fade_in;
-    public bool load_complete;
+    public bool fade_complete;
 
     public float alpha = 0;
     public Color fade_color = new Vector4(1, 1, 1, 1);
@@ -14,58 +14,55 @@ public class LoadingFade : MonoBehaviour
 
     private int child_count;
     public bool fading;
-    public bool fade_state;
+    public bool set_visible;
 
     public int fade_delay;
 
     public bool loaded_in;
+    Scene current_scene;
 
-    // Start is called before the first frame update
     void Start()
     {
         fade_color = GetComponent<SpriteRenderer>().color;
+        DontDestroyOnLoad(gameObject);
 
-        fade_delay = 100;
+        current_scene = SceneManager.GetActiveScene();
+        set_visible = false;
+        fade_complete = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!fading)
+        // code below fades alpha in/out based on set_visible bool
+        if (set_visible)
         {
-            if (fade_in)
-            {
-                fade_state = true;
-                fading = true;
-            }
-            else
-            {
-                fade_state = false;
-                fading = true;
-            }
-        }
-
-        if (fade_state)
-        {
-            if(PhotonNetwork.LevelLoadingProgress > 0.96)
+            if(alpha > 1)
             {
                 alpha = 1;
                 fade_color.a = 1;
-                GetComponent<SpriteRenderer>().color = fade_color;
+
                 fading = false;
                 loaded_in = true;
+
+                fade_complete = true;
             }
             else
             {
                 fade_color.a = alpha;
                 alpha += 0.01f;
-                alpha = PhotonNetwork.LevelLoadingProgress;
                 GetComponent<SpriteRenderer>().color = fade_color;
             }
         }
         else
         {
-            if (alpha > 0)
+            if (alpha < 0)
+            {
+                alpha = 0;
+                fade_color.a = 0;
+                GetComponent<SpriteRenderer>().color = fade_color;
+                fading = false;
+            }
+            else
             {
                 if (fade_delay > 0)
                 {
@@ -79,22 +76,39 @@ public class LoadingFade : MonoBehaviour
                     GetComponent<SpriteRenderer>().color = fade_color;
                 }
             }
-            else
+        }
+
+        // if currently fading, change color of sprite and color of child sprites to match fade_color's transparency
+        // otherwise, if fading in is complete and scene has changed, fade back out (to fully transparent)
+        if (fading)
+        {
+            GetComponent<SpriteRenderer>().color = fade_color;
+            transparency.a = fade_color.a;
+            change_children(transparency);
+
+        }
+        else
+        {
+            if (SceneManager.GetActiveScene() != current_scene)
             {
-                alpha = 0;
-                fade_color.a = 0;
-                GetComponent<SpriteRenderer>().color = fade_color;
-                fading = false;
+                //current_scene = SceneManager.GetActiveScene();
+                set_visible = false;
             }
         }
 
-        transparency.a = fade_color.a;
-        change_children(transparency);
-
-        if ((alpha == 0) && load_complete)
+        // if scene has changed and object has faded out, destroy self
+        if ((alpha == 0) && fade_complete)
         {
             Destroy(gameObject);
         }
+    }
+
+    // method to initiate a scene change. Calling script should then check fade_complete == true i.e. when
+    // this object has fully faded in before changing scenes
+    public void change_scene()
+    {
+        set_visible = true;
+        fade_delay = 100;
     }
 
     private void change_children(Color set_color)
