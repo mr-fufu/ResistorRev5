603c480f911d47d4b210d4c639b355f45c809c5f
Samantha Difeo <sdifeo1@gmail.com>
2021-04-26 04:37:50 -0400

    CSV parser to update parts prefab in editor; currently only updates part stats

---
+++ Assets/Scripts/CSVParser.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CSVParser : MonoBehaviour
+{
+    public List<Part> parts = new List<Part>();
+    
+    private static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    private static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    private static char[] TRIM_CHARS = { '\"' };
+
+    // Do to "ExecuteInEditMode" above, this is called by the Editor, not on start
+#if UNITY_EDITOR
+    
+    [ContextMenu("UPDATE PREFABS FROM CSV")]
+    private void Start()
+    {
+        LoadStatData();
+        //WARNING: this updates the prefabs
+        foreach(var part in parts)
+        {
+            part.UpdatePrefab();
+        }
+    }
+#endif
+    
+    public static List<Dictionary<string, object>> Read(string file)
+    {
+        var list = new List<Dictionary<string, object>>();
+        TextAsset data = Resources.Load(file) as TextAsset;
+
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+
+        if (lines.Length <= 1) return list;
+
+        var header = Regex.Split(lines[0], SPLIT_RE);
+        for (var i = 1; i < lines.Length; i++)
+        {
+
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
+
+            var entry = new Dictionary<string, object>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
+
+    public void LoadStatData()
+    {
+        parts.Clear();
+
+        List<Dictionary<string, object>> data = Read("Stats");
+        for (int i = 0; i < data.Count; i++)
+        {
+            string name = data[i]["PART NAME"].ToString();
+            string manu = data[i]["MANUFACTURER"].ToString();
+            string desc = data[i]["DESC"].ToString();
+            string type = data[i]["TYPE"].ToString();
+            string id = data[i]["ID"].ToString();
+
+            if(name.Equals("PLACEHOLDER"))
+                continue;
+
+            try
+            {
+                int plate = Convert.ToInt32(data[i]["PLATE"]);
+                int logic = Convert.ToInt32(data[i]["LOGIC"]);
+                int range = Convert.ToInt32(data[i]["RANGE"]);
+                int armor = Convert.ToInt32(data[i]["ARMOR"]);
+                int speed = Convert.ToInt32(data[i]["SPEED"]);
+                int fuel = Convert.ToInt32(data[i]["FUEL"]);
+                int power = Convert.ToInt32(data[i]["POWER"]);
+                int slots = Convert.ToInt32(data[i]["SLOTS"]);
+                int cost = Convert.ToInt32(data[i]["COST"]);
+                
+                parts.Add(new Part(name, manu, desc, type, id,
+                    plate, logic, range, armor, speed, fuel, power, slots, cost));
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Sam: one of your stats is NOT an int in the csv. " +
+                                  "Please make sure they all are and there are NO empty strings either, use 0 for the stats.");
+                //throw;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Part.cs
@@ -0,0 +1,79 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+public class Part
+{
+    public string name;
+    public string manufacturer;
+    public string description;
+    public string type;
+    public string id;
+    
+    public int plate;
+    public int logic;
+    public int range;
+    public int armor;
+    public int speed;
+    public int fuel;
+    public int power;
+    public int slots;
+    public int cost;
+    
+    public Part(string name, string manu, string desc, string type, string id,
+                int plate, int logic, int range, int armor, int speed, int fuel, int power, int slots, int cost)
+    {
+        //part general info
+        this.name = name;
+        this.manufacturer = manu;
+        this.description = desc;
+        this.type = type;
+        this.id = id;
+        
+        //part's stats
+        this.plate = plate;
+        this.logic = logic;
+        this.range = range;
+        this.armor = armor;
+        this.speed = speed;
+        this.fuel = fuel;
+        this.power = power;
+        this.slots = slots;
+        this.cost = cost;
+    }
+
+    public void UpdatePrefab()
+    {
+        GameObject prefab = GetAssociatedPrefab();
+        if (prefab == null)
+            return;
+        
+        PartStats stats = prefab.GetComponent<PartStats>();
+
+        stats.partType = type;
+        stats.PLATE = plate;
+        stats.LOGIC = logic;
+        stats.RANGE = range;
+        stats.ARMOR = armor;
+        stats.SPEED = speed;
+        stats.FUEL = fuel;
+        stats.POWER = power;
+        stats.COST = cost;
+        
+        //TODO: stats.slots
+    }
+
+    //find prefab in editor, before runtime please
+    private GameObject GetAssociatedPrefab()
+    {
+        string prefabPath = "Assets/Resources/PartPrefabs/" + type + "Prefabs/" + id + ".prefab";
+        GameObject prefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(Object)) as GameObject;
+
+        if (prefab == null)
+        {
+            Debug.LogError("Sam: Error finding prefab: " + name + ". Check Part.cs and csv file. Path: " + prefabPath);
+        }
+
+        return prefab;
+    }
+}
